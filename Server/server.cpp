d5f99843b6ef38556da7fcf47bae6a49a8c9075b
AdrianMinnich <adrianminnich6@gmail.com>
2019-07-16 23:40:59 +0200

    Working chat. Error handling corrected. Two projects merged into one solution

--- Server/server.cpp
+++ Server/server.cpp
@@ -8,7 +8,7 @@
 
 #define BUF_LENGTH 512
 
-const int MAX_CLIENTS = 3;
+const int MAX_CLIENTS = 5; // TO DO: retire from this
 
 struct Client
 {
@@ -17,12 +17,30 @@ struct Client
 	std::string nick;
 };
 
-int thread_client(Client &new_client, std::vector<Client> clients, std::thread &thread);
+int thread_client(Client &new_client, std::vector<Client> &clients, std::thread &thread);
 
 int main(int argc, char *argv[])
 {
+	if (argc != 3)
+	{
+		std::cout << "Run Server with 2 parameters: IP address and port.\n";
+		return 1;
+	}
+
 	// init winsock
 	WSADATA wsaData;
+	addrinfo hint; // hold host address and socket type information
+	addrinfo *server = NULL;
+	SOCKET server_socket = INVALID_SOCKET;
+	std::vector<Client> clients(MAX_CLIENTS);
+	//std::vector<Client> clients;
+	std::thread threads[MAX_CLIENTS]; //vector instead of an array
+	//std::vector<std::thread> threads;
+
+	std::string msg = "";
+	int num_clients = 0;
+	int temp_id = -1;
+
 	std::cout << "Initializing winsock.\n";
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 	{
@@ -31,7 +49,6 @@ int main(int argc, char *argv[])
 	}
 
 	// setup hint
-	addrinfo hint; // hold host address and socket type information
 	ZeroMemory(&hint, sizeof(hint));
 	hint.ai_family = AF_INET; // IPv4 address family
 	hint.ai_socktype = SOCK_STREAM; // socket type for TCP
@@ -39,17 +56,16 @@ int main(int argc, char *argv[])
 	hint.ai_flags = AI_PASSIVE; // address used in bind()
 
 	// setup server
-	addrinfo *server = NULL;
 	std::cout << "Setting up server.\n";
 	getaddrinfo(static_cast<LPCTSTR>(argv[1]), argv[2], &hint, &server); //argv[1] - IP, argv[2] - port
 
 	// create listening socket
-	SOCKET server_socket = INVALID_SOCKET;
 	std::cout << "Creating server socket.\n";
 	server_socket = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
 
 	// setup socket options
-	//setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, "1", sizeof(int)); // not required
+	setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, "1", sizeof(int));
+	//setsockopt(server_socket, IPPROTO_TCP, TCP_NODELAY, "1", sizeof(int));
 
 	// bind address to socket
 	std::cout << "Binding socket.\n";
@@ -60,30 +76,24 @@ int main(int argc, char *argv[])
 	listen(server_socket, SOMAXCONN);
 
 	// initialize the client list
-	std::vector<Client> clients(MAX_CLIENTS);
-
 	for (int i = 0; i < MAX_CLIENTS; i++)
+	{
 		clients[i] = { -1, INVALID_SOCKET };
-
-	// threads for clients
-	std::thread threads[MAX_CLIENTS];
-
+	}
+ 
 	// wait for requests
-	std::string msg = "";
-	int num_clients = 0;
-	int temp_id = -1;
-
 	while (1)
 	{
 		SOCKET incoming = INVALID_SOCKET;
 		incoming = accept(server_socket, NULL, NULL);
 
-		if (incoming == INVALID_SOCKET)
-			continue;
+		if (incoming == INVALID_SOCKET) continue;
 
+		//Reset the number of clients
 		num_clients = -1;
-		temp_id = -1;
 
+		//Create a temporary id for the next client
+		temp_id = -1;
 		for (int i = 0; i < MAX_CLIENTS; i++)
 		{
 			if (clients[i].socket == INVALID_SOCKET && temp_id == -1)
@@ -110,7 +120,7 @@ int main(int argc, char *argv[])
 			send(clients[temp_id].socket, msg.c_str(), strlen(msg.c_str()), 0);
 
 			// create thread process for every client which receives message from this client and sends it to other clients
-			threads[temp_id] = std::thread(thread_client, std::ref(clients[temp_id]), ref(clients), ref(threads[temp_id]));
+			threads[temp_id] = std::thread(thread_client, std::ref(clients[temp_id]), std::ref(clients), std::ref(threads[temp_id]));
 		}
 		else
 		{
@@ -137,7 +147,7 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-int thread_client(Client &new_client, std::vector<Client> clients, std::thread &thread)
+int thread_client(Client &new_client, std::vector<Client> &clients, std::thread &thread)
 {
 	std::string msg = "";
 	char tempmsg[BUF_LENGTH] = "";
