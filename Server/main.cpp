cef885e8bbf9553635c869c359a906ebcdfe4986
AdrianMinnich <adrianminnich6@gmail.com>
2019-07-16 15:28:19 +0200

    Receiving nick, Sending ID to client and multithreading added

--- Server/main.cpp
+++ Server/main.cpp
@@ -10,23 +10,23 @@
 
 const int MAX_CLIENTS = 3;
 
-using namespace std;
-
 struct Client
 {
 	int id;
 	SOCKET socket;
-	string nick;
+	std::string nick;
 };
+
+int thread_client(Client &new_client, std::vector<Client> clients, std::thread &thread);
 
 int main(int argc, char *argv[])
 {
 	// init winsock
 	WSADATA wsaData;
-	cout << "Initializing winsock.\n";
+	std::cout << "Initializing winsock.\n";
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 	{
-		cout << "Can't initialize winsock. Aborting.\n";
+		std::cout << "Can't initialize winsock. Aborting.\n";
 		return 1;
 	}
 
@@ -40,34 +40,36 @@ int main(int argc, char *argv[])
 
 	// setup server
 	addrinfo *server = NULL;
-	cout << "Setting up server.\n";
+	std::cout << "Setting up server.\n";
 	getaddrinfo(static_cast<LPCTSTR>(argv[1]), argv[2], &hint, &server); //argv[1] - IP, argv[2] - port
 
 	// create listening socket
 	SOCKET server_socket = INVALID_SOCKET;
-	cout << "Creating server socket.\n";
+	std::cout << "Creating server socket.\n";
 	server_socket = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
 
 	// setup socket options
-	setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, "1", sizeof(int)); // not required
+	//setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, "1", sizeof(int)); // not required
 
 	// bind address to socket
-	std::cout << "Binding socket.\n" << std::endl;
+	std::cout << "Binding socket.\n";
 	bind(server_socket, server->ai_addr, (int)server->ai_addrlen);
 
 	// listen for incoming connections
-	std::cout << "Listening.\n" << std::endl;
+	std::cout << "Listening.\n";
 	listen(server_socket, SOMAXCONN);
 
 	// initialize the client list
-	vector<Client> clients(MAX_CLIENTS);
+	std::vector<Client> clients(MAX_CLIENTS);
 
 	for (int i = 0; i < MAX_CLIENTS; i++)
 		clients[i] = { -1, INVALID_SOCKET };
 
+	// threads for clients
+	std::thread threads[MAX_CLIENTS];
 
 	// wait for requests
-	string msg = "";
+	std::string msg = "";
 	int num_clients = 0;
 	int temp_id = -1;
 
@@ -79,7 +81,7 @@ int main(int argc, char *argv[])
 		if (incoming == INVALID_SOCKET)
 			continue;
 
-		num_clients = 0;
+		num_clients = -1;
 		temp_id = -1;
 
 		for (int i = 0; i < MAX_CLIENTS; i++)
@@ -98,12 +100,23 @@ int main(int argc, char *argv[])
 		if (temp_id != -1)
 		{
 			// receive nick from client
+			char nickname[BUF_LENGTH] = "";
+			recv(clients[temp_id].socket, nickname, BUF_LENGTH, 0);
+			clients[temp_id].nick = nickname;
+
 			// send the id to client
-			// create thread process which receives message and sends it to other clients
+			std::cout << clients[temp_id].nick << " connected to the server" << std::endl;
+			msg = std::to_string(clients[temp_id].id);
+			send(clients[temp_id].socket, msg.c_str(), strlen(msg.c_str()), 0);
+
+			// create thread process for every client which receives message from this client and sends it to other clients
+			threads[temp_id] = std::thread(thread_client, std::ref(clients[temp_id]), ref(clients), ref(threads[temp_id]));
 		}
 		else
 		{
-
+			msg = "Server is full";
+			send(incoming, msg.c_str(), strlen(msg.c_str()), 0);
+			std::cout << msg << std::endl;
 		}
 	}
 
@@ -113,6 +126,7 @@ int main(int argc, char *argv[])
 	// close client sockets
 	for (int i = 0; i < MAX_CLIENTS; i++)
 	{
+		threads[i].detach();
 		closesocket(clients[i].socket);
 	}
 
@@ -120,5 +134,60 @@ int main(int argc, char *argv[])
 	WSACleanup();
 
 	system("PAUSE");
+	return 0;
 }
 
+int thread_client(Client &new_client, std::vector<Client> clients, std::thread &thread)
+{
+	std::string msg = "";
+	char tempmsg[BUF_LENGTH] = "";
+
+	while (1)
+	{
+		memset(tempmsg, 0, BUF_LENGTH); //?
+
+		if (new_client.socket != 0)
+		{
+			int iResult = recv(new_client.socket, tempmsg, BUF_LENGTH, 0);
+
+			if (iResult != SOCKET_ERROR)
+			{
+				if (strcmp("", tempmsg))
+					msg = "<" + new_client.nick + "> " + tempmsg;
+
+				std::cout << msg.c_str() << std::endl;
+
+				// broadcast message to other clients
+				for (int i = 0; i < MAX_CLIENTS; i++)
+				{
+					if (clients[i].socket != INVALID_SOCKET)
+						if (new_client.id != i)
+							iResult = send(clients[i].socket, msg.c_str(), strlen(msg.c_str()), 0);
+				}
+
+			}
+			else
+			{
+				msg = new_client.nick + " has disconnected from the server.";
+				std::cout << msg << std::endl;
+
+				closesocket(new_client.socket);
+				closesocket(clients[new_client.id].socket);
+				clients[new_client.id].socket = INVALID_SOCKET;
+
+				// broadcast the disconnection message to other clients
+				for (int i = 0; i < MAX_CLIENTS; i++)
+				{
+					if (clients[i].socket != INVALID_SOCKET)
+						iResult = send(clients[i].socket, msg.c_str(), strlen(msg.c_str()), 0);
+				}
+
+				break;
+			}
+		}
+	}
+
+	thread.detach();
+
+	return 0;
+}
